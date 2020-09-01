8ed916a6e94cf48ddde747fca665bf3c8c489f04
Isaac Overacker <isaac@overacker.me>
2020-09-01 00:20:23 -0700

    Update theming to be properly typed! 🎉

--- src/themes/emotion-native-extended.d.ts
+++ src/themes/emotion-native-extended.d.ts
@@ -1,7 +1,6 @@
-// Noodling around with declaration merging but can't get it to work.
-// import 'emotion-native-extended';
-// import { Theme as MyTheme } from './Theme';
+import '@emotion/react';
+import { Theme as MyTheme } from './Theme';
 
-// declare module 'emotion-native-extended' {
-//     export type Theme = MyTheme;
-// }
+declare module '@emotion/react' {
+    export interface Theme extends MyTheme {}
+}
